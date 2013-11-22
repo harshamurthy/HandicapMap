85ad5ea201fb60ee205313174ff58b0c56cdb6fd
harshamurthy <harshamurthy2@gmail.com>
2013-11-22 14:30:33 -0600

    All Routes

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -8,7 +8,7 @@ class DirectionsController < ApplicationController
     @direction.destination = params[:destination]
 
     if @direction.start.present? && @direction.destination.present?
-      @routes = @direction.accessible?
+      @routes = @direction.directions?
       render 'route'
     else
       redirect_to root_url, notice: 'Please make sure both fields are completed.'
