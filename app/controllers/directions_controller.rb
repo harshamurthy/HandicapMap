3b6c1a5391d6b49c3ecaf577ad2d805b7a7e35eb
harshamurthy <harshamurthy2@gmail.com>
2013-11-25 13:23:42 -0600

    Accessible Routes Only

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -8,7 +8,7 @@ class DirectionsController < ApplicationController
     @direction.destination = params[:destination]
 
     if @direction.start.present? && @direction.destination.present?
-      @routes = @direction.directions?
+      @routes = @direction.accessible?
       render 'route'
     else
       redirect_to root_url, notice: 'Please make sure both fields are completed.'
